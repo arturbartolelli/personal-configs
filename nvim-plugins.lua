3f805e7b7010984d2ce8d5ba7a7dff72a7a0e24e
Artur Bartolelli <125420940+arturbartolelli@users.noreply.github.com>
2025-01-20 10:55:47 -0300

    Update nvim-plugins.lua

--- nvim-plugins.lua
+++ nvim-plugins.lua
@@ -1,24 +1,40 @@
 return {
-    {
-        "projekt0n/github-nvim-theme",
-        opts = {
-        },
-        config = function(_, opts)
-            require("github-theme").setup(opts)
-            vim.cmd("colorscheme github_dark_default")
-        end,
+  {
+    "projekt0n/github-nvim-theme",
+    opts = {
     },
+    config = function(_, opts)
+      require("github-theme").setup(opts)
+    end,
+  },
 
-    -- Fuzzy Finder (Telescope)
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {
-            defaults = {
-                mappings = {
-                    i = { ["<C-u>"] = false, ["<C-d>"] = false },
-                },
-            },
+  -- Fuzzy Finder (Telescope)
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      defaults = {
+        mappings = {
+          i = { ["<C-u>"] = false, ["<C-d>"] = false },
         },
+      },
     },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+       require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+  },
+  {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.g.everforest_background = 'soft'
+        vim.g.everforest_enable_italic = false
+      end
+   }
 }
+
+
